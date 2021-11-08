2ca8e2088edf96726539f0553cfb28c5471c10fc
kgs9843 <74673298+kgs9843@users.noreply.github.com>
2021-11-08 23:39:14 +0900

    Update 2577.c

--- BOJ/2577.c
+++ BOJ/2577.c
@@ -1,16 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 
 int main()
-{
+{	
+	int c[100];
+	int c2[10]={0,};
 	int x=1;
 	int a;
 	for(int i=0;i<3;i++){
 		scanf("%d",&a);
 		x=x*a;
 	}
-	printf("%d\n",x);
-	char c[100];
-	sprintf(c,"%d",x);
-	printf("%s",c[0]);
+	//printf("%d",x);
+	int share=1,count=0;
+	int bb=x;
+	while(x/share!=0){
+		c[count]=bb%10;
+		bb=bb/10;
+		share=share*10;
+		count=count+1;
+	}
+	for(int j=0;j<=9;j++){
+		int final=0;
+		for(int k=0;k<count;k++){
+			if(j==c[k]){
+				final++;
+			}
+		}
+		c2[j]=final;
+		printf("%d\n",c2[j]);
+	}	
 }
