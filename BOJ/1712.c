ed08a555f3dd5e6363f74b513a2fb158a434de16
kgs9843 <74673298+kgs9843@users.noreply.github.com>
2021-11-16 22:45:17 +0900

    Update 1712.c

--- BOJ/1712.c
+++ BOJ/1712.c
@@ -1,19 +1,22 @@
 #include<stdio.h>
 int main(){
-	unsigned int a,b,c;
-	scanf("%d %d %d",&a,&b,&c);
-	if(b>c){
+	unsigned long long int a,b,c;
+	scanf("%llu %llu %llu",&a,&b,&c);
+	if(b>=c){
 		printf("-1");
 		return 0;
 	}
-	unsigned long long int answer=1;
-	unsigned long long int bb=b;
-	unsigned long long int cc=c;
-	while(a+b>=c){
-		b=b+bb;
-		c=c+cc;
-		answer++;
+	if(a+b<c){
+		printf("1");
+		return 0;
 	}
-	printf("%llu",answer);
+	else if(a+b==c){
+		printf("2");
+		return 0;
+	}
+	unsigned long long int answer=1;
+	unsigned long long int process=c-b;
+	printf("%llu",a/process+1);
 	return 0;
+	//1 30 30
 }
