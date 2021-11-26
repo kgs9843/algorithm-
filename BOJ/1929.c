ad727647496a2524727440563fa8a8cbf8df11d9
kgs9843 <74673298+kgs9843@users.noreply.github.com>
2021-11-26 22:36:30 +0900

    Update 1929.c

--- BOJ/1929.c
+++ BOJ/1929.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 int main()
 {
-	//int m,n;
-	//scanf("%d %d",&m,&n);
-	int u[1000001]={0,};
-	int qu=0;
+	int m,n;
+	scanf("%d %d",&m,&n);
+	int u[2000000]={0,};
+	int qu;
 	int i,j;
-	u[0]=-1;
-	for(i=1;i<=100;i++){
+	for(i=1;i<=n;i++){
 		if(i<2){
 			u[i]=-1;
 		}
-		if(u[i]=='-1'){
+		if(u[i]==-1){
 			continue;
 		}
-		printf("%d ",i);
+		//printf("%d ",i);
+		qu=0;
 		j=2;
-		while(qu<=100){
+		while(qu<=n){
 			qu=i*j;
 			u[qu]=-1;
 			j++;
 		}
+	}
+	for(i=m;i<=n;i++){
+		if(u[i]!=-1)printf("%d\n",i);
 	}
 	return 0;
 }
