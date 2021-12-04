38c6e4b9e1696c3ee5f76cf32ccf32ebc944e845
kgs9843 <74673298+kgs9843@users.noreply.github.com>
2021-12-04 22:56:49 +0900

    Update 2447.c

--- BOJ/2447.c
+++ BOJ/2447.c
@@ -1,28 +1,52 @@
 #include <stdio.h>
 char a[2500][2500]={NULL};
-int n,c;
-int i,j;
+int n,c,z1,z2;
+int i,j,k;
 int star(n,c){
 	if(n<3)return 0;
-	for(i=0;i<c;i++){
-		for(j=0;j<c;j++){
-			if((i>=n/3||i<=(n/3)+(n/3)-1)&&(j>=n/3||j<=(n/3)+(n/3)-1)){
-				a[i][j]=" ";
-			}
-			else{
-				a[i][j]='*';
+	else if(n==c){
+		for(i=0;i<c;i++){
+			for(j=0;j<c;j++){
+				if((i>=n/3&&i<=(n/3)+(n/3)-1)&&(j>=n/3&&j<=(n/3)+(n/3)-1)){
+					a[i][j]=' ';
+				}
+				else{
+					a[i][j]='*';
+				}
 			}
 		}
+		star(n/3,c);
 	}
-	star(n/3);
+	else{
+		for(i=0;i<n;i++){
+			for(j=0;j<n;j++){
+				if((i>=n/3&&i<=(n/3)+(n/3)-1)&&(j>=n/3&&j<=(n/3)+(n/3)-1)){
+					z1=i;
+					z2=j;
+					while(i<c){
+						while(j<c){
+							a[i][j]=' ';
+							j=j+n;
+						}
+						j=z2;
+						i=i+n;
+					}
+					i=z1;
+				}
+			}
+		}
+		star(n/3,c);
+	}
 }
 int main(){
 	scanf("%d",&n);
 	c=n;
-	int i;
-	printf("%d",star(n,c));
+	star(n,c);
+	
+	for(i=0;i<c;i++){
+		for(j=0;j<c;j++){
+			printf("%c",a[i][j]);
+		}
+		printf("\n");
+	}
 }
-
-27  1 4 7 10 13 16 19 22 25    4  13 
-9   1 4 7 
-3   1
