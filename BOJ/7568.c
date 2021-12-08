2f706e6258f72132cb8473bf4ef025ef59cb7735
kgs9843 <74673298+kgs9843@users.noreply.github.com>
2021-12-08 23:31:54 +0900

    Update 7568.c

--- BOJ/7568.c
+++ BOJ/7568.c
@@ -6,72 +6,24 @@ int main()
 	scanf("%d",&n);
 	int weghit[50]={0,};
 	int tall[50]={0,};
-	int record[50][2]={0,};
 	int answer[50]={0,};
-	int i,j,k;
+	int i,j;
 	for(i=0;i<n;i++){
 		scanf("%d %d",&weghit[i],&tall[i]);
 	}
-	int count=1;
-	for(j=0;j<n;j++){
-		int max1=0,max2=0;
-		for(i=0;i<n;i++){
-			if(weghit[i]>max1){
-				max1=weghit[i];
-			}
-			if(tall[i]>max2){
-				max2=tall[i];
-			}
-		}
-		for(i=0;i<n;i++){
-			if(max1!=0){
-				if(weghit[i]==max1){
-					record[i][0]=count;
-					weghit[i]=0;
-				}
-			}
-			if(max2!=0){
-				if(tall[i]==max2){
-					record[i][1]=count;
-					tall[i]=0;
-				}
-			}
-		}
-		count++;
-	}
+	int count;
 	for(i=0;i<n;i++){
-		printf("%d %d\n",record[i][0],record[i][1]);
-	}
-	count=1;
-	int add;
-	for(i=0;i<n;i++){
-		if(record[i][0]==count){
-			if(record[i][1]==count){
-				answer[i]=count;
-				record[i][0]=0;
-				record[i][1]=0;
+		count=0;
+		for(j=0;j<n;j++){
+			if(j==i)continue;
+			if(weghit[i]<weghit[j]&&tall[i]<tall[j]){
 				count++;
 			}
-			else{
-				add=0;
-				for(j=0;j<n;j++){
-					if(record[j][1]==0){
-						continue;
-					}
-					else if(record[j][1]<=record[i][1]){
-						answer[j]=count;
-						add++;
-						record[j][0]=0;
-						record[j][1]=0;
-					}
-				}
-				count=count+add;
-			}
 		}
+		answer[i]=count+1;
 	}
 	for(i=0;i<n;i++){
 		printf("%d ",answer[i]);
 	}
 	return 0;
 }
-
