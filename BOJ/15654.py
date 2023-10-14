2fd9478745c917216074050cf9b8fd0126ff8a90
kgs9843 <74673298+kgs9843@users.noreply.github.com>
2023-10-14 13:51:32 +0900

    Create 15654.py

---
+++ BOJ/15654.py
@@ -0,0 +1,16 @@
+def par(array,M):
+    for i in range(len(array)):
+        if(M==1):
+            yield str(array[i])
+        else:
+            for j in par(array[:i]+array[i+1:],M-1):
+                yield str(array[i])+ " " + j
+
+
+
+
+N,M = map(int,input().split())
+array = list(map(int,input().split()))
+array.sort()
+for i in par(array,M):
+    print(i)
